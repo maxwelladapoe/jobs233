79144ef9650b684d83d85b0ce8f4bde38509053a
Maxwell Adapoe <maxwelladapoe@gmail.com>
2020-10-22 05:50:52 +0000

    validation icon fix again

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,14 +13,14 @@
     <link rel="stylesheet" href="{{mix('/css/app.css')}}">
 
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-
+    <script type="module" src="https://unpkg.com/ionicons@5.2.3/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule="" src="https://unpkg.com/ionicons@5.2.3/dist/ionicons/ionicons.js"></script>
 
 </head>
 <body>
 <div id="app">
     <app></app>
 </div>
-<script src="https://unpkg.com/ionicons@5.2.3/dist/ionicons.js"></script>
 <script src="/js/manifest.js"></script>
 <script src="/js/vendor.js"></script>
 <script src="{{mix('/js/app.js')}}"></script>
