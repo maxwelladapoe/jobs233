2ef87d672774ec12f88aca983325517bc9cbbd85
Maxwell Adapoe <maxwelladapoe@gmail.com>
2020-12-08 19:54:57 +0000

    some modifications

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,33 +11,33 @@ const mix = require('laravel-mix');
  |
  */
 mix.disableSuccessNotifications();
-
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
-
-
-//
 //
 // mix.js('resources/js/app.js', 'public/js')
-//     .extract([
-//         'vue',
-//         'axios',
-//         'buefy',
-//         'vue-infinite-loading',
-//         'vee-validate',
-//         'pusher-js',
-//         'laravel-echo',
-//         'vue-timeago',
-//         'vue-router',
-//         'vue-meta',
-//         'vuex',
-//         'vuex-persistedstate',
-//         'vue-i18n',
-//         'vue-pluralize',
-//         'lodash',
-//     ])
-//     .sass('resources/sass/vendor.scss', './public/css/vendor.css')
-//     .sass('resources/sass/app.scss', 'public/css')
-//     .version();
-//
+//     .sass('resources/sass/app.scss', 'public/css');
+
+
+
+
+mix.js('resources/js/app.js', 'public/js')
+    .extract([
+        'vue',
+        'axios',
+        'buefy',
+        'vue-infinite-loading',
+        'vee-validate',
+        'pusher-js',
+        'laravel-echo',
+        'vue-timeago',
+        'vue-router',
+        'vue-meta',
+        'vuex',
+        'vuex-persistedstate',
+        'vue-i18n',
+        'vue-pluralize',
+        'lodash',
+    ])
+    .sass('resources/sass/vendor.scss', './public/css/vendor.css')
+    .sass('resources/sass/app.scss', 'public/css')
+    .version();
+
 
