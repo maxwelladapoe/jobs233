0024cfd21983e6a434e7af39cf7f9d580af69255
Maxwell Adapoe <maxwelladapoe@gmail.com>
2021-01-20 09:30:33 +0000

    correcting 3 dots

--- resources/js/components/extras/ProjectComponent.vue
+++ resources/js/components/extras/ProjectComponent.vue
@@ -140,7 +140,7 @@
         ],
         methods: {
             truncate(str, n) {
-                return str.length > n ? str.substr(0, n - 1) + "&hellip;" : str;
+                return str.length > n ? str.substr(0, n - 1) + "..." : str;
             },
         }
 
